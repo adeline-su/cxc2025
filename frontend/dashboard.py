07bf765cd77ed8908be9154be7adc7c96f41be3b
Adeline Su <su.adeline@gmail.com>
2025-02-23 23:17:01 -0500

    Start streamlit and add one graph

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -1,14 +1,27 @@
+# import streamlit as st
+
+# st.write("# hello world")
+# x = st.text_area("fav movie")
+# st.write(f"ur fav movie is {x}")
+
+# # Initialize y in session state if it doesn't exist
+# if "y" not in st.session_state:
+#     st.session_state.y = 0
+
+# if st.button("Press me"):
+#     st.session_state.y += 1
+
+# st.write(f"Button pressed? {st.session_state.y}")
+
+
 import streamlit as st
+import plotly.io as pio
 
-st.write("# hello world")
-x = st.text_area("fav movie")
-st.write(f"ur fav movie is {x}")
+# Event Types Hierarchy
+with open("notebooks/Event_Types_Sankey_Diagram.json", "r") as f:
+    fig = pio.from_json(f.read())
+st.plotly_chart(fig)
 
-# Initialize y in session state if it doesn't exist
-if "y" not in st.session_state:
-    st.session_state.y = 0
 
-if st.button("Press me"):
-    st.session_state.y += 1
 
-st.write(f"Button pressed? {st.session_state.y}")
+
