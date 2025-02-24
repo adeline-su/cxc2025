d85d34e868fe73ccd1ce320b5fdfa3ed944bc074
Adeline Su <su.adeline@gmail.com>
2025-02-24 00:49:17 -0500

    add more of julia's visualizations

--- frontend/pages/1_insights.py
+++ frontend/pages/1_insights.py
@@ -5,24 +5,28 @@ st.set_page_config(layout="wide")
 
 st.write("## Insights")
 
-# Distribution_of_Number_of_Events
-st.image('notebooks/Distribution_of_Number_of_Events.png')
 
-# Scatter_Plot_of_Number_of_Events_vs_Elapsed_Time
-st.image('notebooks/Scatter_Plot_of_Number_of_Events_vs_Elapsed_Time.png')
-
-st.image('notebooks/julia_eda.png')
+st.write("### Preliminary Insights")
+st.image('notebooks/julia_eda_basic_visualizations.png')
+st.image('notebooks/julia_eda_devices_and_os.png')
 
 
-# Event Types Hierarchy
+st.write("### Events")
 with open("notebooks/Event_Types_Sankey_Diagram.json", "r") as f:
     fig3 = pio.from_json(f.read())
 st.plotly_chart(fig3, key="fig3")
 
+st.image('notebooks/Distribution_of_Number_of_Events.png')
+st.image('notebooks/Scatter_Plot_of_Number_of_Events_vs_Elapsed_Time.png')
 
-st.image('notebooks/Weekly_Retention_Cohorts.png')
+st.write("### Session Length")
+
+st.image('notebooks/julia_eda_session_length.png')
+st.image('notebooks/julia_eda_session_length_distribution.png')
+st.image('notebooks/julia_eda_retention.png')
+
+
 st.image('notebooks/Feature_Correlation_with_Retention.png')
-st.image('notebooks/Session_Duration_Distribution.png')
 st.image('notebooks/Top_Events_in_Long_Sessions.png')
 
 with open("notebooks/Filtered_Event_Flowchart_with_Likeliest_Transition_Percentages.json", "r") as f:
